8e3e4cfb80106ef57a7be1cf12f0d584c19a058c
Chris <hello@chrisashwalker.com>
2024-03-24 14:18:11 +0000

    Refactoring

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 # .doc to .docx file converter
 This Python utility uses the Microsoft Word COM API to convert the .doc files in a directory and its subdirectories into .docx files.
 
+## Things to be aware of:
+- I highly recommend taking a backup of the directory that you will run the conversion on.
+- The program will ask you if you want to delete the original .doc files. 
+- Don't use Word whilst the program is running.
+- It's not fast. The Word COM API is quite slow to perform file I/O operations, so depending on how many files you need to convert, it could take a while. 
+
 ## How to use it:
 Open Command Prompt in the program directory and run the following commands.
 
--- docx-converter.py
+++ docx-converter.py
@@ -1,58 +1,127 @@
 import os
 import sys
+from tqdm import tqdm
 from win32com import client
 
-def convert():
-    dir = ''
-    while dir == '':
-        dir = input('Enter the top level folder path containing the .doc files to convert, or enter quit: \n')
-    if dir.lower() == 'quit':
-        sys.exit()
-    word = client.Dispatch('Word.Application')
-    word.Visible = False
 
-    success_docs = []
-    failed_docs = []
-    unwanted_docs = []
+class Converter:
+    def convert(self, filepath, new_ext):
+        try:
+            new_filepath = ''
+            return new_filepath
+        finally:
+            raise RuntimeError('"Converter.convert" method has not been implemented.')
 
-    for dirpath, dirs, files in os.walk(dir):
+
+# Uses Microsoft Word to change file formats
+class WordConverter(Converter):
+    def __init__(self):
+        # The Word API has an enumeration of file formats. We'll define those that we may need.
+        self.file_types = {
+            '.doc': 0,
+            '.docx':  16
+        }
+    
+    def __enter__(self):
+        # Create instance of COM object
+        self.client = client.Dispatch('Word.Application')
+        self.client.Visible = False
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.client.Quit()
+
+    # Overriding superclass method
+    def convert(self, filepath, new_ext):
+        doc = None
+        try:
+            doc = self.client.Documents.Open(filepath)
+            new_filepath = os.path.splitext(filepath)[0] + new_ext
+            doc.SaveAs2(new_filepath, self.file_types[new_ext])
+            return new_filepath
+        finally:
+            if doc:
+                doc.Close()
+
+
+def deleteFiles(files):
+    successes = []
+    failures = []
+    for file in tqdm(iterable=files, desc='Tidying up...', unit='files'):
+        if os.path.exists(file):
+            try:
+                os.remove(file)
+                if not os.path.exists(file):
+                    successes.append(file)
+                else:
+                    raise Exception('Failed to delete: ' + file)
+            except:
+                failures.append(file)
+    return successes, failures
+
+def getFiles(folder, file_ext):
+    matching_files = []
+    for dirpath, dirnames, files in os.walk(folder):
         for filename in files:
-            if filename[-4:] == '.doc':
-                fullpath = os.path.join(dirpath, filename)
-                doc = None
-                try:
-                    doc = word.Documents.Open(fullpath)
-                    doc.SaveAs2(fullpath + 'x', 16)
-                    success_docs.append(fullpath)
-                except:
-                    failed_docs.append(fullpath)
-                finally:
-                    if doc:
-                        doc.Close()
+            if os.path.splitext(filename)[1] == file_ext:
+                filepath = os.path.join(dirpath, filename)
+                matching_files.append(filepath)
+    return matching_files
 
-    word.Quit()
+def askForFolder():
+    while True:    
+        folder = input('Enter the top level folder path containing the files to convert, or enter quit: \n')
+        if folder.lower() == 'quit':
+            sys.exit()
+        if folder != '':
+            break
+    return folder
 
-    delete_docs = ''
-    while not delete_docs.lower() in ['y', 'n']:
-        delete_docs = input('Delete the converted .doc files? Y or N: \n')
+def askIfDeleting():
+    while True:    
+        deleting = input('Delete the converted files? Y or N: \n')
+        if deleting.lower() in ['y', 'n']:
+            return deleting.lower() == 'y'
 
-    if delete_docs == 'y':
-        for doc_to_delete in success_docs:
-            if os.path.exists(doc_to_delete):
-                try:
-                    os.remove(doc_to_delete)
-                except:
-                    unwanted_docs.append(doc_to_delete)
+def main():
+    # Word file specifics
+    old_ext = '.doc'
+    new_ext = '.docx'
+    converter = WordConverter()
 
-    if len(failed_docs) > 0:
-        print('Warning - These docs failed to convert: \n')
-        [print(failure + '\n') for failure in failed_docs]
+    processed_files = []
+    rejected_files = []
+    unwanted_files = []
 
-    if len(unwanted_docs) > 0:
-        print('Warning - These docs converted but failed to be deleted: \n')
-        [print(unwanted + '\n') for unwanted in unwanted_docs]
+    folder = askForFolder()
+    deleting_after_conversion = askIfDeleting(); 
+    docs_to_convert = getFiles(folder, old_ext)
+
+    with converter:
+        for file in tqdm(iterable=docs_to_convert, desc='Converting...', unit='files'):
+            try:
+                converted_filepath = converter.convert(file, new_ext)
+                if os.path.exists(converted_filepath):
+                    processed_files.append(file)
+                else:
+                    raise Exception('Failed to convert: ' + file)
+            except:
+                rejected_files.append(file)
+
+    if deleting_after_conversion:
+        deleted_files, unwanted_files = deleteFiles(processed_files)
+
+    if len(rejected_files) > 0:
+        print('Warning - These files failed to convert: \n')
+        [print(failure + '\n') for failure in rejected_files]
+
+    if len(unwanted_files) > 0:
+        print('Warning - These files converted but failed to be deleted: \n')
+        [print(unwanted + '\n') for unwanted in unwanted_files]
 
     input('Done. Press any key to exit.')
 
+
+# Program entry point
 if __name__ == '__main__':
-    convert()
+    main()
